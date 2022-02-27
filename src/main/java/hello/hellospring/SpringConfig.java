763687a91e9be36d042f87da7b4398e3cc81951b
이승원 <squareo073@gmail.com>
2022-02-27 21:58:11 +0900

    Changes

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,6 +1,8 @@
 package hello.hellospring;
 import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,23 +19,32 @@ public class SpringConfig {
         this.dataSource = dataSource;
     }
 */
-
+/*
     private EntityManager em;
 
     public SpringConfig(EntityManager em) {
         this.em = em;
     }
+*/
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(@Qualifier("") MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
+//  @Bean
+//  public MemberRepository memberRepository() {
 //      return new MemoryMemberRepository();
 //      return new JdbcMemberRepository(dataSource);
 //      return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//      return new JpaMemberRepository(em);
+//      return ;
+//  }
 }
